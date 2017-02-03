049a92410da7f64dccf46c9b087877d17684a666
Roberto Vasquez Angel <roberto@vasquez-angel.de>
2017-02-03 21:50:36 +0100

    Refactoring.

--- lib/multi_client/spec_helper.rb
+++
@@ -1,9 +0,0 @@
-module MultiClient
-  module SpecHelper
-    def use_client(client)
-      MultiClient::Client.current_id = client.id
-      Capybara.current_session.driver.reset!
-      Capybara.default_host = Capybara.default_host.sub(/(.*?\/\/)(.*?)(\..*)/, "\\1#{client.subdomain}\\3")      
-    end
-  end
-end
---
+++ lib/multi_client/spec_helpers/feature.rb
@@ -0,0 +1,21 @@
+module MultiClient
+  module SpecHelpers
+    module Feature
+      def with_client(client, &block)
+        MultiClient::Client.with_client(client) { block.call }
+      end
+
+      def use_subdomain(subdomain)
+        Capybara.current_session.driver.reset!
+        Capybara.default_host = Capybara.default_host.sub(/(.*?\/\/)(.*?)(\..*)/, "\\1#{subdomain}\\3")      
+      end
+
+      def with_subdomain(subdomain, &block)
+        original_host = Capybara.default_host
+        use_subdomain(subdomain)
+        yield
+        Capybara.default_host = original_host
+      end
+    end
+  end
+end
