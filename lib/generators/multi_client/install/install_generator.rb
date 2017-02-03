126f531aca7519822d34f8e670c797d0fb34cdea
Roberto Vasquez Angel <roberto@vasquez-angel.de>
2017-02-03 21:49:30 +0100

    Generator now adds app/models/client.rb.

--- lib/generators/multi_client/install/install_generator.rb
+++ lib/generators/multi_client/install/install_generator.rb
@@ -8,6 +8,10 @@ module MultiClient
       def generate_intializer
         copy_file 'initializer.rb', 'config/initializers/multi_client.rb'
       end
+
+      def generate_client_model
+        copy_file 'client.rb', 'app/models/client.rb'
+      end
     end
   end
 end
